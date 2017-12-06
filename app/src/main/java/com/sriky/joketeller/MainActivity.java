07099665ee26f638705685c42ffb9fa6eeade1a1
sriky <srikanthbm@gmail.com>
2017-12-05 22:24:14 -0800

    Feat: Use the backend api to get the jokes, doesn't work yet as the connection request is getting refused.

--- app/src/main/java/com/sriky/joketeller/MainActivity.java
+++ app/src/main/java/com/sriky/joketeller/MainActivity.java
@@ -1,14 +1,30 @@
 package com.sriky.joketeller;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.sriky.jokedisplay.JokeActivity;
 import com.sriky.jokeprovider.JokeProvider;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+import timber.log.Timber;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Timber.plant(new Timber.DebugTree());
     }
 
 
@@ -43,10 +60,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        JokeProvider jokeProvider = new JokeProvider();
+        new FetchJokeTask().execute();
+    }
 
-        Intent intent = new Intent(MainActivity.this, JokeActivity.class);
-        intent.putExtra(JokeActivity.JOKE_INTENT_BUNDLE_KEY, jokeProvider.getJoke());
-        startActivity(intent);
+    class FetchJokeTask extends AsyncTask<Void, Void, String> {
+
+        private MyApi myApiService = null;
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            if(myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        //TODO: set this programmatically.
+                        .setRootUrl("http://136.24.116.14:8080/_ah/api/")
+                        .setApplicationName(getString(R.string.app_name))
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            try {
+                return myApiService.sayHi("It is working, are you joking??? No! HAHHAHAHA!!!").execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(TextUtils.isEmpty(s)) {
+                Timber.e("No joke returned from server!!!");
+                return;
+            }
+
+            Intent intent = new Intent(MainActivity.this, JokeActivity.class);
+            intent.putExtra(JokeActivity.JOKE_INTENT_BUNDLE_KEY, s);
+            startActivity(intent);
+        }
     }
 }
