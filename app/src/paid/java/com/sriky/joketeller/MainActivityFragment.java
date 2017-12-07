e4c9e8970a29480f09002f59d29675db0de2223b
sriky <srikanthbm@gmail.com>
2017-12-07 14:54:28 -0800

    Refactor: Add fragment class and layout specific to the paid version.

---
+++ app/src/paid/java/com/sriky/joketeller/MainActivityFragment.java
@@ -0,0 +1,26 @@
+package com.sriky.joketeller;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
